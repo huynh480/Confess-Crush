c1298a937e546fae047ec4af28495b95aa3bd3a6
huynh480 <106756683+huynh480@users.noreply.github.com>
2022-06-03 18:17:55 +0700

    Fduyn.lun

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "Lời tỏ tình của tôi",
     introTitle: 'Nguyễn Bích Duyên',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `mai ngày anh sẽ nấu cơm
+   Học kho cá vs thom :))`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'cứ trả lời thật lòng nha🥰',
+    desc: ' Chưa được sự cho phép mà đã tự ý thích em.
+ Anh xin lỗi nhé công chúa! ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: ' vậy là đồng ý r nhó.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
